bf12bfdcc363f866799d95dcd154112890ee5938
renankanu <renankanu@gmail.com>
2020-11-10 21:17:54 -0300

    Feature - create bottom bar

--- lib/app/modules/core/core_view.dart
+++ lib/app/modules/core/core_view.dart
@@ -1,10 +1,12 @@
 import 'package:app_restaurant/app/modules/core/widgets/custom_bottom_bar.dart';
+import 'package:app_restaurant/app/utils/custom_colors.dart';
 import 'package:flutter/material.dart';
 
 class CoreView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: CustomColors.mountainMeadow,
       bottomNavigationBar: CustomBottomBar(),
     );
   }
--- lib/app/modules/core/widgets/custom_bottom_bar.dart
+++ lib/app/modules/core/widgets/custom_bottom_bar.dart
@@ -1,8 +1,37 @@
+import 'package:app_restaurant/app/utils/custom_colors.dart';
 import 'package:flutter/material.dart';
 
 class CustomBottomBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.only(
+            topRight: Radius.circular(14), topLeft: Radius.circular(14)),
+        boxShadow: [
+          BoxShadow(color: Colors.black38, spreadRadius: 0, blurRadius: 10),
+        ],
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(14),
+          topRight: Radius.circular(14),
+        ),
+        child: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: CustomColors.white,
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Category',
+                activeIcon: Icon(Icons.access_alarm)),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Restaurants',
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
