89f4a692295a20371a2c917162a43b04c8cb9dcf
renankanu <renankanu@gmail.com>
2020-11-11 22:40:17 -0300

    Feature - revert layout

--- lib/app/modules/core/core_view.dart
+++ lib/app/modules/core/core_view.dart
@@ -1,6 +1,4 @@
-import 'package:app_restaurant/app/modules/core/widgets/custom_base.dart';
 import 'package:app_restaurant/app/modules/core/widgets/custom_bottom_bar.dart';
-import 'package:app_restaurant/app/utils/custom_colors.dart';
 import 'package:flutter/material.dart';
 
 class CoreView extends StatelessWidget {
@@ -12,102 +10,7 @@ class CoreView extends StatelessWidget {
         backgroundColor: Colors.red,
       ),
       backgroundColor: Colors.white,
-      body: Stack(
-        children: [
-          Positioned.fill(
-            top: 50,
-            child: CustomBase(
-              body: Column(
-                children: [
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: CustomColors.black,
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: CustomColors.luckyPoint,
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: CustomColors.black,
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: CustomColors.luckyPoint,
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: CustomColors.black,
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: CustomColors.luckyPoint,
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: CustomColors.black,
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: CustomColors.luckyPoint,
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: CustomColors.black,
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: CustomColors.luckyPoint,
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: CustomColors.black,
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: CustomColors.luckyPoint,
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: CustomColors.black,
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: CustomColors.luckyPoint,
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Positioned(
-            left: 0,
-            right: 0,
-            child: Align(
-              alignment: Alignment.topCenter,
-              child: Container(
-                width: 100,
-                height: 100,
-                color: CustomColors.linkWater,
-              ),
-            ),
-          ),
-        ],
-      ),
-      // bottomNavigationBar: CustomBottomBar(),
+      bottomNavigationBar: CustomBottomBar(),
     );
   }
 }
