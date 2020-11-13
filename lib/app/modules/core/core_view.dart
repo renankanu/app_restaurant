a2ced43266758e48592e85ec629af19d2b23c0f5
Renan Kanu <renankanu@gmail.com>
2020-11-12 23:19:31 -0300

    Feature - create custom bottom navigation

--- lib/app/modules/core/core_view.dart
+++ lib/app/modules/core/core_view.dart
@@ -1,7 +1,15 @@
 import 'package:app_restaurant/app/modules/core/widgets/custom_bottom_bar.dart';
+import 'package:app_restaurant/app/modules/home/views/home_view.dart';
 import 'package:flutter/material.dart';
 
-class CoreView extends StatelessWidget {
+class CoreView extends StatefulWidget {
+  @override
+  _CoreViewState createState() => _CoreViewState();
+}
+
+class _CoreViewState extends State<CoreView> {
+  int _selectedItem = 0;
+  List<Widget> getPage = [HomeView()];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,7 +19,22 @@ class CoreView extends StatelessWidget {
         brightness: Brightness.light,
       ),
       backgroundColor: Colors.white,
-      bottomNavigationBar: CustomBottomBar(),
+      body: getPage[_selectedItem],
+      bottomNavigationBar: CustomBottomBar(
+        iconList: [
+          Icons.home,
+          Icons.card_giftcard,
+          Icons.camera,
+          Icons.pie_chart,
+          Icons.person,
+        ],
+        onChange: (val) {
+          setState(() {
+            _selectedItem = val;
+          });
+        },
+        defaultSelectedIndex: 1,
+      ),
     );
   }
 }
