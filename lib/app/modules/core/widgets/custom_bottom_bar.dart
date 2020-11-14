567e3d1d975e9367187137993bc2ed860090c717
renankanu <renankanu@gmail.com>
2020-11-14 14:16:57 -0300

    Feature - change bottom add border radius

--- lib/app/modules/core/widgets/custom_bottom_bar.dart
+++ lib/app/modules/core/widgets/custom_bottom_bar.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../../../utils/custom_colors.dart';
+import '../../../utils/custom_colors.dart';
+
 class CustomBottomBar extends StatefulWidget {
   final int defaultSelectedIndex;
   final Function(int) onChange;
   final List<IconData> iconList;
 
-  const CustomBottomBar({this.defaultSelectedIndex = 0, @required this.onChange, @required this.iconList});
+  const CustomBottomBar(
+      {this.defaultSelectedIndex = 0,
+      @required this.onChange,
+      @required this.iconList});
   @override
   _CustomBottomBarState createState() => _CustomBottomBarState();
 }
@@ -31,35 +37,43 @@ class _CustomBottomBarState extends State<CustomBottomBar> {
       _navBarItemList.add(buildNavBarItem(_iconList[i], i));
     }
 
-    return Row(
-      children: _navBarItemList,
+    return Container(
+      decoration: BoxDecoration(
+        color: CustomColors.luckyPoint,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(20),
+          topRight: Radius.circular(20),
+        ),
+      ),
+      child: Row(
+        children: _navBarItemList,
+      ),
     );
   }
 
   Widget buildNavBarItem(IconData icon, int index) {
-    return GestureDetector(
-      onTap: (){
-        widget.onChange(index);
-        setState(() {
-          _selectedIndex = index;
-        });
-      },
-      child: Container(
-        height: 60,
-        width: MediaQuery.of(context).size.width / _iconList.length,
-        decoration: index == _selectedIndex ? BoxDecoration(
-            border: Border(
-              bottom: BorderSide(width: 4, color: Colors.green),
-            ),
-            gradient: LinearGradient(colors: [
-              Colors.green.withOpacity(0.3),
-              Colors.green.withOpacity(0.015),
-            ], begin: Alignment.bottomCenter, end: Alignment.topCenter)
-          // color: index == _selectedItemIndex ? Colors.green : Colors.white,
-        ) : BoxDecoration(),
-        child: Icon(
-          icon,
-          color: index == _selectedIndex ? Colors.black : Colors.grey,
+    return Container(
+      child: GestureDetector(
+        onTap: () {
+          widget.onChange(index);
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        child: Container(
+          height: 60,
+          width: MediaQuery.of(context).size.width / _iconList.length,
+          decoration: index == _selectedIndex
+              ? BoxDecoration(
+                  border: Border(
+                    top: BorderSide(width: 4, color: CustomColors.persimmon),
+                  ),
+                )
+              : BoxDecoration(),
+          child: Icon(
+            icon,
+            color: index == _selectedIndex ? Colors.black : Colors.grey,
+          ),
         ),
       ),
     );
