5d79585ac286b0a27dce377db89be0e157ab590f
renankanu <renankanu@gmail.com>
2020-11-16 21:12:24 -0300

    Refactor - create request restaurants[ok]

--- lib/app/modules/core/controllers/core_controller.dart
+++ lib/app/modules/core/controllers/core_controller.dart
@@ -1,11 +1,11 @@
-import 'package:app_restaurant/app/modules/detail/views/detail_view.dart';
 import 'package:app_restaurant/app/modules/home/views/home_view.dart';
+import 'package:app_restaurant/app/modules/restaurants/views/restaurants_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class CoreController extends GetxController {
   var selectedItem = 0.obs;
-  final List<Widget> getPage = [HomeView(), DetailView()];
+  final List<Widget> getPage = [HomeView(), RestaurantsView()];
 
   void changeIndexPage(index) => selectedItem.value = index;
 
